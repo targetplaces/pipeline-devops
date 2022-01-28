8a6b3805e0d3b07c8924c6c2b56f10041b3e53fa
targetplaces <luchoz.dd@gmail.com>
2022-01-28 18:39:53 -0300

    Configuracion JEnkinsFile

---
+++ vars/ejecucion.groovy
@@ -0,0 +1,45 @@
+def call {
+	pipeline {
+		agent any
+		
+		environment {
+			STAGE = ''
+		}
+		
+		parameters {
+		  choice choices: ['gradle', 'maven'], description: 'indicar herramienta de construcción', name: 'buildTool'
+		}
+
+		stages {
+			stage('pipeline'){
+				steps{
+					script{
+						println 'pipeline'
+						println params.buildTool
+						
+
+						if (params.buildTool=='gradle') {
+								def ejecucion = load 'gradle.groovy'
+								ejecucion.call()
+						} else {
+								def ejecucion = load 'maven.groovy'
+								ejecucion.call()
+						}
+
+
+					}
+				}
+			}
+			
+		}
+		post {
+			success{
+				slackSend(color: 'good', channel: "U02MBA9FXHD", message: "[${env.BUILD_USER}][${env.JOB_NAME}][${params.buildTool}] Ejecución Exitosa.")
+			}
+			failure {
+				slackSend(color: 'danger', channel: "U02MBA9FXHD", message: "[${env.BUILD_USER}][${env.JOB_NAME}][${params.buildTool}] Ejecución fallida en Stage: ${STAGE}")
+			}
+		}
+	}
+}
+return this;
